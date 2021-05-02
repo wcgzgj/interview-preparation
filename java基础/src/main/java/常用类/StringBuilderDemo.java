7d0cacce7ef74ced8e79372b953317e374097fe8
wcgzgj <zhangjia_good@126.com>
2021-05-02 17:43:49 +0800

    StringBuilder

---
+++ java基础/src/main/java/常用类/StringBuilderDemo.java
@@ -0,0 +1,17 @@
+package 常用类;
+
+/**
+ * @ClassName StringBuilderDemo
+ * @Description TODO
+ * @Author FARO_Z
+ * @Date 2021/5/2 下午5:36
+ * @Version 1.0
+ **/
+public class StringBuilderDemo {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("锄禾日当午");
+        sb.append("汗滴禾下土");
+        System.out.println(sb.toString());
+    }
+}
