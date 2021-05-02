58391167ae1562fc4e77d98c69fd309d84f6fcff
wcgzgj <zhangjia_good@126.com>
2021-05-02 17:36:05 +0800

    String

---
+++ java基础/src/main/java/常用类/StringDemo.java
@@ -0,0 +1,19 @@
+package 常用类;
+
+/**
+ * @ClassName StringDemo
+ * @Description TODO
+ * @Author FARO_Z
+ * @Date 2021/5/2 下午5:12
+ * @Version 1.0
+ **/
+public class StringDemo {
+    public static void main(String[] args) {
+
+        String text1 = "锄禾日当午";
+        String text2 = "汗滴禾下土";
+        String text3 = "床前明月光";
+        text1=text1+text2+text3;
+        System.out.println(text1);
+    }
+}
