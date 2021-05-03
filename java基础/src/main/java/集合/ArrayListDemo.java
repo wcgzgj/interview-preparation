c99336d92c1e89e6f0e916939edf095b4c12adde
wcgzgj <zhangjia_good@126.com>
2021-05-03 15:02:14 +0800

    ArrayListDemo

---
+++ java基础/src/main/java/集合/ArrayListDemo.java
@@ -0,0 +1,20 @@
+package 集合;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @ClassName ArrayListDemo
+ * @Description TODO
+ * @Author FARO_Z
+ * @Date 2021/5/3 上午2:04
+ * @Version 1.0
+ **/
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        new ArrayList<>();
+        new LinkedList<>();
+    }
+}
