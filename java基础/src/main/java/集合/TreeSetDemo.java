cfc0eb071bd75d1c6bf13b3aeabec8b911ef9b01
wcgzgj <zhangjia_good@126.com>
2021-05-03 16:11:47 +0800

    TreeSet

---
+++ java基础/src/main/java/集合/TreeSetDemo.java
@@ -0,0 +1,72 @@
+package 集合;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * @ClassName TreeSetDemo
+ * @Description TODO
+ * @Author FARO_Z
+ * @Date 2021/5/3 下午3:39
+ * @Version 1.0
+ **/
+public class TreeSetDemo {
+    public static void main(String[] args) {
+        Set<Person> set = new TreeSet<>();
+        Person p1 = new Person("张三", 20);
+        Person p2 = new Person("李四", 19);
+        Person p3 = new Person("麻子", 19);
+
+        set.add(p1);
+        set.add(p2);
+        set.add(p3);
+
+        for (Person person : set) {
+            System.out.println(person);
+        }
+
+    }
+
+}
+
+class Person implements Comparable<Person> {
+    private String name;
+    private Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return this.age-o.getAge();
+    }
+}
